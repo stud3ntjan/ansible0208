8702e3207ffedab5bcc74643e304c603cce56a8a
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-08-02 15:18:16 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "eu-central-1"
+  access_key    = var.aws_access_key
+  secret_key    = var.aws_secret_key
+    region = "eu-central-1"
 }
 
 resource "aws_instance" "web" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "aws_session_token" {}
 
 variable "region" {
   default = "eu-central-1"
