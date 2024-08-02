5fe6342204c0e205289b8a97f9c42b07827c0d64
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-08-02 15:29:21 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key    = var.aws_access_key
   secret_key    = var.aws_secret_key
-    region = "eu-central-1"
+    region = var.region
 }
 
 resource "aws_instance" "web" {
