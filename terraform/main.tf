42786ff6229fbcf44416dd9ccbab6028dc404682
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-08-02 14:17:42 +0200

    create ini & yml

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-013efd7d9f40467af" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  key_name = "key" 
+}
+
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "eu-central-1"
+}
+variable "ami" {
+  default = "ami-013efd7d9f40467af"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "key_name" {}
